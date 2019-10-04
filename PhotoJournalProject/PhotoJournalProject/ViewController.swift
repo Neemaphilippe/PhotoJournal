f93ed47783cde78ab7ce01a6bee1f5e81db6e736
Pursuit <pursuit@Pursuits-MacBook-Pro-3.local>
2019-10-04 17:58:13 -0400

    Added second VC and files

--- PhotoJournalProject/PhotoJournalProject/ViewController.swift
+++ PhotoJournalProject/PhotoJournalProject/ViewController.swift
@@ -10,6 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var photoView: UICollectionView!
+    
+    
+    @IBAction func addButtonPressed(_ sender: Any) {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let addPhotos = storyboard.instantiateViewController(withIdentifier: "addPhotoEntryVC") as? addPhotoEntryVC
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
---
+++ PhotoJournalProject/PhotoJournalProject/addPhotoEntryVC.swift
@@ -0,0 +1,37 @@
+//
+//  addPhotoEntryVC.swift
+//  PhotoJounralProject
+//
+//  Created by Pursuit on 10/4/19.
+//  Copyright © 2019 Neema Philippe. All rights reserved.
+//
+
+import UIKit
+
+class addPhotoEntryVC: UIViewController {
+    
+    @IBAction func cancelButtonPressed(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func saveButtonPressed(_ sender: UIButton) {
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
