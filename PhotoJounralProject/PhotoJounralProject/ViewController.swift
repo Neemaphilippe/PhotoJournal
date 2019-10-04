85e9e47d7a2f8d051395b9386252793ee41cd8ca
Neemaphilippe <52180399+Neemaphilippe@users.noreply.github.com>
2019-10-04 16:45:37 -0400

    Add files via upload

---
+++ PhotoJounralProject/PhotoJounralProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PhotoJounralProject
+//
+//  Created by Pursuit on 10/4/19.
+//  Copyright © 2019 Neema Philippe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
